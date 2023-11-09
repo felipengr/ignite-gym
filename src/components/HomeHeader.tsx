afbfd93c2da86230672463b78f350003df5c9fe1
Felipe Nogueira <felipe.nogueira@maeztra.com>
2023-11-09 11:37:12 -0300

    user photo component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,24 @@
+import { HStack, Heading, Text, VStack } from "native-base";
+import { UserPhoto } from "./UserPhoto";
+
+export function HomeHeader() {
+    return (
+        <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+            <UserPhoto
+                source={{ uri: 'https://github.com/felipengr.png' }}
+                size={16}
+                alt="Imagem do usuário"
+                mr={4}
+            />
+            <VStack>
+                <Text color="gray.100" fontSize="md">
+                    Olá,
+                </Text>
+
+                <Heading color="gray.100" fontSize="md">
+                    Felipe
+                </Heading>
+            </VStack>
+        </HStack>
+    )
+}

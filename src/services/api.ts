138c21986163f58a7b098f9edf6e3cc76e5ebbd6
Felipe Nogueira <felipe.nogueira@maeztra.com>
2023-11-24 02:25:13 -0300

    integração com backend via axios

---
+++ src/services/api.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { AppError } from "@utils/AppError";
+
+const api = axios.create({
+    baseURL: 'http://192.168.7.106:3333'
+})
+
+api.interceptors.response.use(response => response, error => {
+    if (error.response && error.response.data) {
+        return Promise.reject(new AppError(error.response.data.message))
+    } else {
+        return Promise.reject(new AppError('Erro no servidor. Tente novamente mais tarde'))
+    }
+})
+
+export { api }
